a08eeb20c6b55036a142b61d28afb4cc7427c4ae
ShivanshuCSE24 <au530329@gmail.com>
2026-09-10 10:20:48 +0530

    Time: 38 ms (5.94%), Space: 54.5 MB (11.22%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -2,37 +2,37 @@ import java.util.*;
 
 class Solution {
 
-    Map<String, Integer> dp = new HashMap<>();
+    Map<String, Integer> mp = new HashMap<>();
 
-    int fun(int i, String s, Map<String, Integer> m1, String tmp) {
+    int fun(int i, String s, Map<String, Integer> m1, String temp) {
 
         if (i >= s.length()) {
-            System.out.println(tmp);
+            System.out.println(temp);
 
-            if (tmp.length() == 0)
+            if (temp.length() == 0)
                 return 1;
 
             return 0;
         }
 
-        String key = Integer.toString(i) + "+" + tmp;
+        String key = Integer.toString(i) + "+" + temp;
 
-        if (dp.containsKey(key))
-            return dp.get(key);
+        if (mp.containsKey(key))
+            return mp.get(key);
 
         int m = 0;
 
-        tmp += s.charAt(i);
+        temp += s.charAt(i);
 
-        if (m1.containsKey(tmp)) {
+        if (m1.containsKey(temp)) {
             int a = fun(i + 1, s, m1, "");
             m = m | a;
         }
 
-        int a = fun(i + 1, s, m1, tmp);
+        int a = fun(i + 1, s, m1, temp);
         m = m | a;
 
-        dp.put(key, m);
+        mp.put(key, m);
 
         return m;
     }
@@ -45,7 +45,7 @@ class Solution {
             m1.put(a, m1.getOrDefault(a, 0) + 1);
         }
 
-        dp.clear();
+        mp.clear();
 
         return fun(0, s, m1, "") == 1;
     }
