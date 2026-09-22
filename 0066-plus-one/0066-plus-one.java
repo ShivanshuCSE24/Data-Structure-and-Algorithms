361357cd45d816ae20d51910f4583fb14a36dfe6
ShivanshuCSE24 <au530329@gmail.com>
2026-09-22 21:05:35 +0530

    Time: 0 ms (100%), Space: 43.3 MB (61.33%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int len = digits.length - 1;
+
+        while(len >= 0){
+            if(digits[len] < 9){
+
+                digits[len]++;
+                return digits;
+
+            }
+            else{
+                digits[len] = 0;
+            }
+            len--;
+        }
+
+        int[] result = new int[digits.length + 1];
+
+        result[0] = 1;
+        return result;
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+
+//             int len = digits.length -1;
+
+//             while(len >= 0){
+
+//                 if(digits[len] < 9){
+//                     digits[len]++;
+//                     return digits;
+//                 }
+//                 else{
+//                     digits[len] = 0;
+//                 }
+//                 len--;
+//         }  
+
+    
+
+//         int result[] = new int[digits.length + 1];
+//         result[0] = 1;
+
+//         return result;
+
+
+//  }}
